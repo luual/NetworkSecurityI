209b21873a5e94ab32e969045ae7c9dea1efe52c
alexpug <alexandreluu6@gmail.com>
2016-11-16 23:04:35 +0100

    add packet test integrity

--- tests/RepositoryTest.cpp
+++ tests/RepositoryTest.cpp
@@ -6,7 +6,7 @@
 **
 ** Creation Date : mer. 16 nov. 2016 21:58:24 CET
 **
-** Last Modified : mer. 16 nov. 2016 22:32:29 CET
+** Last Modified : mer. 16 nov. 2016 23:04:15 CET
 **
 ** Created by : Alexandre LUU <https://github.com/luual>
 **
@@ -21,4 +21,14 @@ bool PushRepositoryTest()
     Packet p1;
     repo->Insert(p1);
     return repo->Size() == 1;
+}
+
+bool PacketIntegrityTest()
+{
+    IRepository<Packet> *repo = new VectorRepository<Packet>;
+    Packet p1;
+    p1.ipHeader.version = 4;
+    repo->Insert(p1);
+    Packet p2 = repo->GetByIndex(0);
+    return p1.ipHeader.version == p2.ipHeader.version;
 }
--- tests/main_test.cpp
+++ tests/main_test.cpp
@@ -6,7 +6,7 @@
 **
 ** Creation Date : lun. 14 nov. 2016 22:17:33 CET
 **
-** Last Modified : mer. 16 nov. 2016 22:33:50 CET
+** Last Modified : mer. 16 nov. 2016 23:04:17 CET
 **
 ** Created by : Alexandre LUU <https://github.com/luual>
 **
@@ -14,12 +14,17 @@
 
 #include <cassert>
 
+// ========== Repository test ==========
 bool PushRepositoryTest();
+bool PacketIntegrityTest();
+// ========== END Repository test ==========
+
 int ThreadConcurrency();
 
 int main(void)
 {
     //ThreadConcurrency();
     assert(PushRepositoryTest() == 1);
+    assert(PacketIntegrityTest() == 1);
     return 0;
 }
